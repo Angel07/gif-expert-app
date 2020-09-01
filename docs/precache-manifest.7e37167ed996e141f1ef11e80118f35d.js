6fada79b62cba01d90dad19dea1aeb93ab8f10d7
_avargas7 <angelvargastarrillo@hotmail.com>
2020-08-31 23:59:14 -0500

    first commit

---
+++ docs/precache-manifest.7e37167ed996e141f1ef11e80118f35d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d506931c359536aa9fd9f0e5add519",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d709f6205b5fc64f59e9",
+    "url": "/static/css/main.5e7e3507.chunk.css"
+  },
+  {
+    "revision": "9948515d998a14a1480d",
+    "url": "/static/js/2.6a91807f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a91807f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d709f6205b5fc64f59e9",
+    "url": "/static/js/main.1246c327.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
